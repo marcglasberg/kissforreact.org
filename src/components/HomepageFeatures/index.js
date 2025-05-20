84a2076f4a5431c802a8596a532e243fc7155ba6
Marcelo Glasberg <13332110+marcglasberg@users.noreply.github.com>
2025-05-20 17:18:04 -0300

    Doc improvements.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,25 +2,25 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import Link from "@docusaurus/Link";
-import {useState} from "react";
+import { useState } from "react";
 import OverviewContent from './OverviewContent'; // Import our MDX wrapper component instead
 
 const FeatureList = [];
 
-function Feature({Svg, title, page, description}) {
+function Feature({ Svg, title, page, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img"/>
+        <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className={styles.getStartedTextAndLogo}>
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         <Link
           className="button button--primary button--lg"
-          style={{marginBottom: 50}}
+          style={{ marginBottom: 50 }}
           to={`/${page}/intro`}>
           Get Started
         </Link>
@@ -33,7 +33,7 @@ export function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row" style={{display: 'flex', justifyContent: 'center'}}>
+        <div className="row" style={{ display: 'flex', justifyContent: 'center' }}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
@@ -50,11 +50,12 @@ export function Overview(page) {
     <div>
       <section className={styles.features}>
 
-        <div className="container">          <div className={styles.overview}>
+        <div className="container">
+          <div className={styles.overview}>
             <OverviewContent />
           </div>
 
-          <div style={{height: 40}}></div>
+          <div style={{ height: 40 }}></div>
           <center>
             <iframe
               src="https://codesandbox.io/embed/j57yz5?view=split&module=%2Fsrc%2FApp.tsx&hidenavigation=1&fontsize=12.0&editorsize=75&previewwindow=browser"
@@ -79,14 +80,14 @@ export function Overview(page) {
           }}>
             <Link
               className="button button--primary button--lg"
-              style={{marginBottom: 5, alignItems: 'center'}}
+              style={{ marginBottom: 5, alignItems: 'center' }}
               to={`/${activeTab}/intro`}>
-              <span style={{display: 'inline-block', transform: 'translateY(-6px)'}}>
+              <span style={{ display: 'inline-block', transform: 'translateY(-6px)' }}>
                 Get Started &nbsp;<span style={{
-                fontSize: 30,
-                display: 'inline-block',
-                transform: 'translateY(2px)'
-              }}>»</span>
+                  fontSize: 30,
+                  display: 'inline-block',
+                  transform: 'translateY(2px)'
+                }}>»</span>
               </span>
             </Link>
           </div>
